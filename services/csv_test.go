2ebacb444aa3ebcb9bccb1a5b1227cda96008144
Javier <sjavierlopez@gmail.com>
2021-01-16 22:11:32 -0600

    Fix test

--- services/csv_test.go
+++ services/csv_test.go
@@ -26,7 +26,7 @@ func TestPointService(t *testing.T) {
 	})
 
 	t.Run("Should return a list of endpoints correctly", func(t *testing.T) {
-		file := "data/puntos_examen_fullstack.csv"
+		file := "../data/puntos_examen_fullstack.csv"
 		testcsv := NewCsvService()
 		result, err := testcsv.ParseCSVtoPoint(file, true)
 		if err != nil {
@@ -37,8 +37,8 @@ func TestPointService(t *testing.T) {
 		assert.Equal(t, "0101000020E6100000609F8C33BED959C0F5E83676C3893940", record1.TheGeom)
 		assert.Equal(t, 1, record1.CartoDBID)
 		assert.Equal(t, "Sucursal", record1.Type)
-		assert.Equal(t, 25.53813876, record1.Latitude)
-		assert.Equal(t, -103.40223397, record1.Longitude)
+		assert.Equal(t, "25.53813876", record1.Latitude)
+		assert.Equal(t, "-103.40223397", record1.Longitude)
 		assert.Equal(t, "11A579", record1.Color)
 	})
 }
